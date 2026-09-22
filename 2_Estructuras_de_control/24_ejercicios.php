1bebe4369213fcd6468724143739d79949eea714
Alevflb <alejandrov99@gmail.com>
2026-09-22 10:22:43 +0200

    ª

--- 2_Estructuras_de_control/24_ejercicios.php
+++ 2_Estructuras_de_control/24_ejercicios.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Ejercicio 1. Crea una función llamada edad que haciendo uso de la estructura de control switch muestre por pantalla si una persona es menor de edad, adulta, jubilada o anciana. adulta va de 18 a 67 años y jubilada de 67 en adelante. Hacer una llamada a la función con un número random del 1 al 100. IMPORTANTE: Controlar que el número NUNCA pueda ser negativo.</p>
+    <p>Ejercicio 1. Crea una función llamada edad que haciendo uso de la estructura de control switch muestre por pantalla si una persona es menor de edad, adulta, jubilada o anciana. adulta va de 18 a 67 años, jubilada de 67 hasta los 80 y anciana de 80 en adelante. Hacer una llamada a la función con un número random del 1 al 100. IMPORTANTE: Controlar que el número NUNCA pueda ser negativo.</p>
 
     <p>
         Ejercicio 2. Crear una función llamada notas que contenga un parámetro decimal. Si la nota es menor que 5, se mostrará por pantalla la palabra "suspenso". Si la nota está entre 5 y 6, se mostrará "Aprobado", si está entre 7 y 8 --> "Notable" y si es 9 o 10 --> "Sobresaliente".
